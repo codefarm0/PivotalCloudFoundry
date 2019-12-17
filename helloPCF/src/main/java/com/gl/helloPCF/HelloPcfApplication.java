6926a9eee35f70e09d4336bbe4ee4d98765f75b1
Green Learner <greenlearner01@users.noreply.github.com>
2019-12-17 10:45:27 +0530

    new changes in ppt and jar to push to pcf

--- helloPCF/src/main/java/com/gl/helloPCF/HelloPcfApplication.java
+++ helloPCF/src/main/java/com/gl/helloPCF/HelloPcfApplication.java
@@ -1,17 +1,34 @@
 package com.gl.helloPCF;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 @RestController
 public class HelloPcfApplication {
 
+	private Logger logger = LoggerFactory.getLogger(HelloPcfApplication.class);
+
+	private String openTag = "<h2 align = center> <font color=green>";
+	private String closeTag = "</font></h2>";
 	@GetMapping("/hello")
 	public String helloPCFWorld(){
-		return "!!Hello PCF!!";
+		logger.info("Got hit on /hello");
+		return openTag + "!!Hello PCF!! " + closeTag;
+	}
+
+	@GetMapping("/ipAddress")
+	public String getIpAddress() throws UnknownHostException {
+		logger.info("Got hit on /ipAddress");
+		String host = InetAddress.getLocalHost().getHostAddress();
+		return openTag + "I am running on machine - "  + host + closeTag;
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(HelloPcfApplication.class, args);
